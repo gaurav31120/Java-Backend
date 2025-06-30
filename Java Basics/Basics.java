40f72cd28a596ecb41738d8268b847731fd615f3
Gaurav Kumar <gaurav31120@gmail.com>
2025-06-30 07:21:02 +0530

    2 List

---
+++ Java Basics/Basics.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basics {
+
+    public static void main(String[] args) {
+        
+        List<String> arr = new ArrayList<>(); //define
+        int[] arr1 = {1,2,3};
+        char[] arr2 = {'a','b','c'};
+
+        for(int i=0; i<arr1.length; i++) {
+            System.out.println(arr1[i]);
+        }
+        for(int i=0; i<arr2.length; i++) {
+            System.out.println(arr2[i]);
+        }
+        // output: 
+        // 1
+        // 2
+        // 3
+        // a
+        // b
+        // c
+    }
+}
